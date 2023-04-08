ccd0e871dde1e75715e0561c1657e122536fc63b
Anton Ermakov <anton.ermakov@gameteq.com>
2023-04-09 03:40:24 +0400

    Add PriceCount Enum, Edit PerkType enum

--- Assets/Scripts/Enums/PerkType.cs
+++ Assets/Scripts/Enums/PerkType.cs
@@ -3,18 +3,30 @@ namespace Enums
     public enum PerkType
     {
         Undefined,
+
         #region Currency
-        StartCurrency1Max,
+
+        Currency0Add,
+        Currency0Max,
+        Currency1Open,
         Currency1Add,
+        Currency1Max,
         Currency2Open,
         Currency2Add,
+        Currency2Max,
         Currency3Open,
         Currency3Add,
-        AutoConvert,
+        Currency3Max,
+        Currency4Open,
+        Currency4Add,
+        Currency4Max,
+
         #endregion
+
         #region Workers
-        StartMinersLvl1Count,
-        MinersLvl1
+
+        StartMinersLvl1Count
+
         #endregion
     }
 }
---
+++ Assets/Scripts/Enums/PriceCount.cs
@@ -0,0 +1,11 @@
+namespace Enums
+{
+    public enum PriceCount
+    {
+        Undefined,
+        Currency0Count,
+        Currency1Count,
+        Currency2Count,
+        Currency3Count,
+    }
+}
